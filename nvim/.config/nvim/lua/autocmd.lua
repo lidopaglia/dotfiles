5e96b9a937eeedbb1c51a4a79d3bc524ece7a8a8
Lido Paglia <lido@paglia.org>
2021-12-03 16:43:20 -0500

    prevent newlines to also start with comment

--- nvim/.config/nvim/lua/autocmd.lua
+++ nvim/.config/nvim/lua/autocmd.lua
@@ -1,2 +1,7 @@
--- Remove all trailing whitespace on save
 -- Prevent new line to also start with a comment
+vim.api.nvim_exec([[
+  augroup NewLineComment
+    au!
+    au FileType * setlocal formatoptions-=c formatoptions-=r formatoptions-=o
+  augroup END
+  ]], false)
