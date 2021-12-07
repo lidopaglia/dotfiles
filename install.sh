5f73a530b46f58dc655f67fa6965fd6e37c99d9f
Lido Paglia <lido@paglia.org>
2021-12-07 18:40:49 -0500

    include install.sh

---
+++ install.sh
@@ -0,0 +1,25 @@
+# install nix
+curl -L https://nixos.org/nix/install | sh
+
+# source nix
+. ~/.nix-profile/etc/profile.d/nix.sh
+
+# install packages
+nix-env -iA \
+    nixpkgs.bat \
+    nixpkgs.curl \
+    nixpkgs.delta \
+    nixpkgs.direnv \
+    nixpkgs.fzf \
+    nixpkgs.gcc \
+    nixpkgs.git \
+    nixpkgs.gnumake \
+    nixpkgs.kitty \
+    nixpkgs.neovim \
+    nixpkgs.ripgrep \
+    nixpkgs.stow \
+    nixpkgs.tmux \
+    nixpkgs.tree \
+    nixpkgs.wget \
+    nixpkgs.zoxide \
+    nixpkgs.zsh
