36a2baeaf2a7ede7efbb53b96093cb7611b24f49
Lido Paglia <lido@paglia.org>
2024-05-12 11:29:35 -0400

    update wezterm config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,3 +1,7 @@
+-- WezTerm Config
+-- https://wezfurlong.org/wezterm/config/files.html
+
+
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
@@ -6,18 +10,25 @@ config.enable_tab_bar = false
 config.window_padding = {
   left = 20,
   right = 20,
-  top = 20,
-  bottom = 20,
+  top = 10,
+  bottom = 10,
 }
+
+config.window_decorations = "RESIZE"
 
 config.bold_brightens_ansi_colors = true
 
-config.font =
-  wezterm.font_with_fallback {
+config.color_scheme = 'Tomorrow Night'
+
+config.colors = {
+  background = 'black',
+}
+
+config.font = wezterm.font_with_fallback {
     'Ubuntu Mono',
     'Symbols Nerd Font Mono'
   }
 
-config.font_size = 13
+config.font_size = 18
 
 return config
