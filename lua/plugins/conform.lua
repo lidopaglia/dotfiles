ed68879c3dac696f297a8fa4ee3ef0bdb8d32b90
Lido Paglia <lido@paglia.org>
2025-08-01 15:37:20 -0400

    update conform config

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,60 +1,46 @@
 return {
   -- https://github.com/stevearc/conform.nvim
   "stevearc/conform.nvim",
-  config = function()
-    require("conform").setup({
-
-      formatters_by_ft = {
-        sh = { "shfmt" },
-        lua = { "stylua" },
-        css = { "prettier" },
-        html = { "prettier" },
-        json = { "prettier" },
-        yaml = { "prettier" },
-        markdown = { "prettier" },
-      },
-
-      -- command to toggle format-on-save
-      -- https://github.com/stevearc/conform.nvim/blob/master/doc/recipes.md#command-to-toggle-format-on-save
-      format_on_save = function(bufnr)
-        -- Disable with a global or buffer-local variable
-        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-          return
-        end
-        return { timeout_ms = 500, lsp_format = "fallback" }
+  event = { "BufWritePre" },
+  cmd = { "ConformInfo" },
+  keys = {
+    {
+      "<leader>cf",
+      function()
+        require("conform").format({ async = true }, function(err, did_edit)
+          if not err and did_edit then
+            vim.notify("Code formatted", vim.log.levels.INFO, { title = "Conform" })
+          end
+        end)
       end,
-    })
+      mode = { "n", "v" },
+      desc = "Format buffer",
+    },
+  },
 
-    -- format on save
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      callback = function(args)
-        require("conform").format({
-          bufnr = args.buf,
-          lsp_fallback = true,
-          quiet = true,
-        })
-      end,
-    })
+  opts = {
 
-    -- command to toggle format-on-save: user commands
-    vim.api.nvim_create_user_command("FormatDisable", function(args)
-      if args.bang then
-        -- FormatDisable! will disable formatting just for this buffer
-        vim.b.disable_autoformat = true
-      else
-        vim.g.disable_autoformat = true
-      end
-    end, {
-      desc = "Disable autoformat-on-save",
-      bang = true,
-    })
+    formatters_by_ft = {
+      sh = { "shfmt" },
+      lua = { "stylua" },
+      css = { "prettier" },
+      html = { "prettier" },
+      json = { "prettier" },
+      yaml = { "prettier" },
+      markdown = { "prettier" },
+    },
 
-    -- command to toggle format-on-save: user commands
-    vim.api.nvim_create_user_command("FormatEnable", function()
-      vim.b.disable_autoformat = false
-      vim.g.disable_autoformat = false
-    end, {
-      desc = "Re-enable autoformat-on-save",
-    })
+    default_format_opts = {
+      lsp_format = "fallback",
+    },
+
+    format_on_save = {
+      timeout_ms = 500,
+      lsp_format = "fallback",
+    },
+  },
+
+  init = function()
+    vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
   end,
 }
