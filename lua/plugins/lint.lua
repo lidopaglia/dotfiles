61b3ad6b8716d252e293b593d688514b5d27bc11
Lido Paglia <lido@paglia.org>
2025-08-01 15:19:28 -0400

    add mason

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -4,8 +4,36 @@ return {
   event = { "BufReadPre", "BufNewFile" },
   config = function()
     local lint = require("lint")
+
+    -- Configure custom linters using Mason-managed tools
+    local mason_bin_dir = vim.fn.stdpath("data") .. "/mason/bin"
+
+    -- Customize golangcilint to ignore exit codes
+    -- (golangci-lint exits with code 1-3 when issues are found)
+    local golangcilint = require("lint").linters.golangcilint
+    golangcilint.ignore_exitcode = true
+
+    -- Configure linters by filetype (using Mason-managed tools)
     lint.linters_by_ft = {
+      -- Go
+      go = { "golangcilint" },
+
+      -- JavaScript/TypeScript
+      javascript = { "eslint_d" },
+      typescript = { "eslint_d" },
+      javascriptreact = { "eslint_d" },
+      typescriptreact = { "eslint_d" },
+
+      -- Lua
+      lua = { "luacheck" },
+
+      -- Markdown
       markdown = { "markdownlint" },
+
+      -- Shell
+      sh = { "shellcheck" },
+      bash = { "shellcheck" },
+      zsh = { "shellcheck" },
     }
 
     -- To allow other plugins to add linters to require('lint').linters_by_ft,
@@ -40,9 +68,9 @@ return {
     -- lint.linters_by_ft['terraform'] = nil
     -- lint.linters_by_ft['text'] = nil
 
-    -- Create autocommand which carries out the actual linting
-    -- on the specified events.
+    -- Perform linting on specified events (save and text changes).
     local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
     vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
       group = lint_augroup,
       callback = function()
@@ -54,5 +82,21 @@ return {
         end
       end,
     })
+
+    -- Manual linting command
+    vim.keymap.set("n", "<leader>ll", function()
+      lint.try_lint()
+      vim.notify("Linting...", vim.log.levels.INFO, { title = "nvim-lint" })
+    end, { desc = "Trigger linting for current file" })
+
+    -- Show linter status
+    vim.keymap.set("n", "<leader>li", function()
+      local linters = lint.linters_by_ft[vim.bo.filetype] or {}
+      if #linters == 0 then
+        print("No linters configured for filetype: " .. vim.bo.filetype)
+      else
+        print("Linters for " .. vim.bo.filetype .. ": " .. table.concat(linters, ", "))
+      end
+    end, { desc = "Show available linters for current filetype" })
   end,
 }
